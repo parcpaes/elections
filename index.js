6e10dcce08c00a8b0b211d8045897b842a9a4f17
paes marca <paesm14@gmail.com>
2020-09-09 18:57:50 -0400

    fix index.js cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const winston = require('winston');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 require('./startup/logging');
 require('./startup/routes')(app);
